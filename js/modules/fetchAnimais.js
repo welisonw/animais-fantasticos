7f1cea1693688d5722ce57a4295828a237edf79f
Welison Wessling <welison.smi@hotmail.com>
2023-05-30 16:45:49 -0300

    Refatoração fetchAnimais

--- js/modules/fetchAnimais.js
+++ js/modules/fetchAnimais.js
@@ -1,36 +1,42 @@
 
 import AnimaNumeros from './animaNumeros.js';  // para carregar a contagem dos números 
 
-export default function initFetchAnimais() {
-   async function fetchAnimais(url) {
+export default function fetchAnimais(url, target) {
+   // Cria a div contendo informações com o total de animais
+   function createAnimalElement(animal) {
+      const div = document.createElement('div');
+      div.classList.add('numero-animal');
+      div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`
+
+      return div;
+   }; 
+
+   // Anima números de cada animal
+   function animaAnimaisNumeros() {
+      const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+
+      animaNumeros.init();
+   };
+   
+   // Fetch animais do arquivo JSON e cria cada animal utilzando a função
+   async function createAnimais() {
       try {
          const animaisResponse = await fetch(url);
          const animaisJSON = await animaisResponse.json();
-         const numerosGrid = document.querySelector('.numeros-grid');
-
-         function createAnimal(animal) {
-            const div = document.createElement('div');
-            div.classList.add('numero-animal');
-      
-            div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`
-      
-            return div;
-         };  
-
+         
+         // Preenche cada animal no DOM
+         const numerosGrid = document.querySelector(target);
          animaisJSON.forEach((animal) => {
-            const divAnimal = createAnimal(animal);
+            const divAnimal = createAnimalElement(animal);
 
             numerosGrid.appendChild(divAnimal);
          });
 
-         const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
-
-         animaNumeros.init();
-
+         animaAnimaisNumeros();   
       } catch (error) {
          console.error(error);
-      }
+      };
    };
 
-   fetchAnimais('./animais-api.json');   
+   return createAnimais();
 };
